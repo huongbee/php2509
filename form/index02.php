e3a87ce14d111a5732fe28bc98cdac1c4685832f
Huong <admin@Apples-MacBook-Pro.local>
2018-10-18 19:58:56 +0700

    btn

--- form/index02.php
+++ form/index02.php
@@ -19,19 +19,52 @@
         width: 100px
     }
 </style>
-
+<?php
+// if(isset($_GET['txtSoa'])){
+//     $a = $_GET['txtSoa'];
+// }
+// else $a = 0;
+if(isset($_GET['btn'])){
+    $a = isset($_GET['txtSoa']) ? $_GET['txtSoa'] : 0;
+    $b = isset($_GET['txtSob']) ? $_GET['txtSob'] : 0;
+    $pt = isset($_GET['txtPheptinh']) ? $_GET['txtPheptinh'] : '';
+    // $kq = '';
+    if($pt=='+'){
+        $kq = $a+$b;
+    }
+    elseif($pt=='-'){
+        $kq = $a-$b;
+    }
+    elseif($pt=='*'){
+        $kq = $a*$b;
+    }
+    elseif($pt=='/'){
+        if($b==0){
+            $kq = "B phải khác 0";
+        }
+        else $kq = $a/$b;
+    }
+    else{
+        if($b==0){
+            $kq = "B phải khác 0";
+        }
+        else $kq = $a%$b;
+    }
+    echo $kq;
+}
+?>
 <body>
-    <form>
-        <input type="text" placeholder="Enter number a">
-        <select>
+    <form method="GET">
+        <input type="text" placeholder="Enter number a" name="txtSoa">
+        <select name="txtPheptinh">
             <option>+</option>
             <option>-</option>
             <option>*</option>
             <option>/</option>
             <option>%</option>
         </select>
-        <input type="text" placeholder="Enter number b">
-        <button>Calculate</button>
+        <input type="text" placeholder="Enter number b" name="txtSob">
+        <button name="btn">Calculate</button>
         <input type="text" placeholder="Result">
     </form>
 </body>
