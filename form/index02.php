b8fcccd4313ced33bce99830cdfa0a6ba2bdf854
Huong <admin@Apples-MacBook-Pro.local>
2018-10-18 20:35:37 +0700

    done

--- form/index02.php
+++ form/index02.php
@@ -18,17 +18,36 @@
     button{
         width: 100px
     }
+    select{
+        width: 120px
+    }
 </style>
 <?php
 // if(isset($_GET['txtSoa'])){
 //     $a = $_GET['txtSoa'];
 // }
 // else $a = 0;
+
 if(isset($_GET['btn'])){
-    $a = isset($_GET['txtSoa']) ? $_GET['txtSoa'] : 0;
-    $b = isset($_GET['txtSob']) ? $_GET['txtSob'] : 0;
+    $a = isset($_GET['txtSoa']) ? (float)$_GET['txtSoa'] : 0;
+    $b = isset($_GET['txtSob']) ? (float)$_GET['txtSob'] : 0;
+    // $a = isset($_GET['txtSoa']) && is_numeric($_GET['txtSoa']) ? $_GET['txtSoa'] : 0;
+    // $b = isset($_GET['txtSob']) && is_numeric($_GET['txtSob']) ? $_GET['txtSob'] : 0;
     $pt = isset($_GET['txtPheptinh']) ? $_GET['txtPheptinh'] : '';
-    // $kq = '';
+    /*
+    switch($pt){
+        case '+':
+            $kq = $a+$b;break;
+        case '-':
+            $kq = $a-$b;break;
+        default:
+            if($b==0){
+                $kq = "B phải khác 0";
+            }
+            else $kq = $a%$b;
+    }
+    */
+
     if($pt=='+'){
         $kq = $a+$b;
     }
@@ -50,22 +69,24 @@ if(isset($_GET['btn'])){
         }
         else $kq = $a%$b;
     }
-    echo $kq;
+    // echo $kq;
 }
 ?>
+
 <body>
     <form method="GET">
-        <input type="text" placeholder="Enter number a" name="txtSoa">
+        <input type="text" value="<?=isset($a)? $a : ''?>" placeholder="Enter number a" name="txtSoa">
         <select name="txtPheptinh">
-            <option>+</option>
-            <option>-</option>
-            <option>*</option>
-            <option>/</option>
-            <option>%</option>
+            <option>Chọn phép tính</option>
+            <option value="+" <?= isset($pt) && $pt=='+' ? 'selected': ''?>>+</option>
+            <option value="-" <?= isset($pt) && $pt=='-' ? 'selected': ''?>>-</option>
+            <option value="*" <?= isset($pt) && $pt=='*' ? 'selected': ''?>>*</option>
+            <option value="/" <?= isset($pt) && $pt=='/' ? 'selected': ''?>>/</option>
+            <option value="%" <?= isset($pt) && $pt=='%' ? 'selected': ''?>>%</option>
         </select>
-        <input type="text" placeholder="Enter number b" name="txtSob">
-        <button name="btn">Calculate</button>
-        <input type="text" placeholder="Result">
+        <input type="text" value="<?=isset($b)? $b : ''?>" placeholder="Enter number b" name="txtSob">
+        <input type="submit" name="btn" value="Calculate"/>
+        <input type="text" placeholder="Result" value="<?=isset($kq)? $kq : ''?>">
     </form>
 </body>
 </html>
