a285f408439b59ae820022fe7c74f64748156029
Huong <admin@Apples-MacBook-Pro.local>
2018-10-20 21:13:10 +0700

    upload multi file

--- form/upload-multi/xuly-upload.php
+++ form/upload-multi/xuly-upload.php
@@ -2,8 +2,24 @@
 
 $file = $_FILES['avatar'];
 
-foreach($file['tmp_name'] as $key=>$tmpName){
+foreach($file['size'] as $key=>$size){
     $name = $file['name'][$key];
+    if($size>1024*1024){
+        echo "File $name too large!";
+        exit;
+    }
+}
+$arrExt = ['png','jpg','gif']; 
+foreach($file['name'] as $key=>$name){
+    $ext = strtolower(pathinfo($name,PATHINFO_EXTENSION));
+    if(!in_array($ext,$arrExt)){
+        echo "File $name not allow!";
+        exit;
+    }
+}
+
+foreach($file['tmp_name'] as $key=>$tmpName){
+    $name = rand(10000,100000).'-'.$file['name'][$key];
     move_uploaded_file($tmpName,"../avatars/$name");
 }
 
@@ -15,7 +31,7 @@ foreach($file['tmp_name'] as $key=>$tmpName){
 // 
 // print_r($file);
 /**
-  * Upload nhiều file cùng lúc
+  * Upload nhiều file cùng lúc, rollback neu co bat ki file nao ko thoa man
   * 1. Chỉ cho phép upload các file hình ảnh (png, jpg, gif)
   * 2. File upload không quá 1 MB
   * 3. Đổi tên file trước khi upload
