0796b679ae42f0c07840cd6a8895f8588b82a78f
Huong <admin@Apples-MacBook-Pro.local>
2018-10-27 21:06:02 +0700

    session cart

--- session/cart/add-to-cart.php
+++ session/cart/add-to-cart.php
@@ -7,16 +7,16 @@ $id = isset($_POST['id']) ? $_POST['id'] : null;
 if($id!==null){
     $sp = findProduct($id,$arraySP);
     if(!empty($sp)){
-        // if(!isset($_SESSION['cart'])){
-        //     $_SESSION['cart'] = [
-        //         $id => $sp
-        //     ];
-        // }
-        // else{
-        //     $_SESSION['cart'][$id] = $sp;
-        // }
+        $_SESSION['cart'] = isset($_SESSION['cart']) ? $_SESSION['cart'] : null;
+
+        if($_SESSION['cart'] != null && array_key_exists($id,$_SESSION['cart'])){
+            $_SESSION['cart'][$id]['qty'] += 1; 
+        }
+        else{
+            $sp['qty'] = 1;
+            $_SESSION['cart'][$id] = $sp;
+        }
         
-        $_SESSION['cart'][$id] = $sp;
         echo 'Add to cart success';
     }
     else{
@@ -29,11 +29,11 @@ else{
 
 
 
-
-
-
-
-
+// [id] => 5
+// [name] =>  iPhone 6 32GB
+// [price] => 7200000
+// [image] => 42/92962/iphone-6-32gb-vang-400-400x460.png
+// [qty] => 1
 
 // echo $_GET['id'];
 // echo (int)$_POST['id'];
